3696307101a6bc9e960113efb3b877b4578a0d1b
Oliver Gratton <ojgratton@gmail.com>
2020-12-26 20:36:31 +0000

    refactor

--- boids.py
+++ boids.py
@@ -7,7 +7,6 @@ which is an adaptation of
 http://www.cs.toronto.edu/~dt/siggraph97-course/cwr87/
 """
 
-import itertools
 import random
 import pygame
 import pygame.locals as pyg
@@ -16,17 +15,11 @@ from pygame.math import Vector3  # TODO replace with numpy
 from OpenGL import GL, GLU
 
 
-def random_range(lower=0.0, upper=1.0):
-    "return a random number between lower and upper"
-    return lower + (random.random() * (upper - lower))
-
-
 def random_vector3(lower=0.0, upper=1.0):
-    "return a Vector3 with random elements between lower and upper"
     return Vector3(
-        random_range(lower, upper),
-        random_range(lower, upper),
-        random_range(lower, upper),
+        random.uniform(lower, upper),
+        random.uniform(lower, upper),
+        random.uniform(lower, upper),
     )
 
 
@@ -35,19 +28,23 @@ class Rule:
     Template for rules of flocking
     """
 
+    NEIGHBOURHOOD = 5  # max distance at which rule is applied
+
     def __init__(self):
-        "initialize the correction base class"
-        self.delta = Vector3(0, 0, 0)  # velocity correction
+        self.change = Vector3(0, 0, 0)  # velocity correction
         self.num = 0  # number of participants
-        self.neighborhood = 5.0  # area for this correction
 
     def accumulate(self, boid, other, distance):
-        "save any corrections based on other boid to self.delta"
-        pass
+        """
+        Save any corrections based on other boid to self.change
+        """
+        raise NotImplementedError
 
     def add_adjustment(self, boid):
-        "add the accumulated self.delta to boid.adjustment"
-        pass
+        """
+        Add the accumulated self.change to boid.adjustment
+        """
+        raise NotImplementedError
 
 
 class Cohesion(Rule):
@@ -55,20 +52,17 @@ class Cohesion(Rule):
     Rule 1: Boids try to fly towards the centre of mass of neighbouring boids.
     """
 
-    def __init__(self):
-        super().__init__()
-
     def accumulate(self, boid, other, distance):
         if other != boid:
-            self.delta = self.delta + other.location
+            self.change = self.change + other.location
             self.num += 1
 
     def add_adjustment(self, boid):
         if self.num > 0:
-            centroid = self.delta / self.num
+            centroid = self.change / self.num
             desired = centroid - boid.location
-            self.delta = (desired - boid.velocity) * 0.0006
-        boid.adjustment = boid.adjustment + self.delta
+            self.change = (desired - boid.velocity) * 0.0006
+        boid.adjustment = boid.adjustment + self.change
 
 
 class Alignment(Rule):
@@ -76,20 +70,18 @@ class Alignment(Rule):
     Rule 2: Boids try to match velocity with near boids.
     """
 
-    def __init__(self):
-        super().__init__()
-        self.neighborhood = 10.0  # operating area for this correction
+    NEIGHBOURHOOD = 10
 
     def accumulate(self, boid, other, distance):
         if other != boid:
-            self.delta = self.delta + other.velocity
+            self.change = self.change + other.velocity
             self.num += 1
 
     def add_adjustment(self, boid):
         if self.num > 0:
-            group_velocity = self.delta / self.num
-            self.delta = (group_velocity - boid.velocity) * 0.03
-        boid.adjustment = boid.adjustment + self.delta
+            group_velocity = self.change / self.num
+            self.change = (group_velocity - boid.velocity) * 0.03
+        boid.adjustment = boid.adjustment + self.change
 
 
 class Separation(Rule):
@@ -101,22 +93,22 @@ class Separation(Rule):
         if other != boid:
             separation = boid.location - other.location
             if separation.length() > 0:
-                self.delta = self.delta + (separation.normalize() / distance)
+                self.change = self.change + (separation.normalize() / distance)
             self.num += 1
 
     def add_adjustment(self, boid):
-        if self.delta.length() > 0:
-            group_separation = self.delta / self.num
-            self.delta = (group_separation - boid.velocity) * 0.01
-        boid.adjustment = boid.adjustment + self.delta
+        if self.change.length() > 0:
+            group_separation = self.change / self.num
+            self.change = (group_separation - boid.velocity) * 0.01
+        boid.adjustment = boid.adjustment + self.change
 
 
 class Boid:
     def __init__(self):
         self.color = random_vector3(0.5)  # R G B
-        self.location = Vector3(0, 0, 0.0)  # x y z
+        self.location = Vector3(0, 0, 0)  # x y z
         self.velocity = random_vector3(-1.0, 1.0)  # vx vy vz
-        self.adjustment = Vector3(0, 0, 0.0)  # to accumulate corrections
+        self.adjustment = Vector3(0, 0, 0)  # to accumulate corrections
 
     def __repr__(self):
         return "color %s, location %s, velocity %s" % (
@@ -148,15 +140,15 @@ class Boid:
         """
         Calculate new position
         """
-        corrections = [Cohesion(), Alignment(), Separation()]
+        rules = [Cohesion(), Alignment(), Separation()]
         for other in others:  # accumulate corrections
             distance = self.location.distance_to(other.location)
-            for correction in corrections:
-                if distance < correction.neighborhood:
-                    correction.accumulate(self, other, distance)
+            for rule in rules:
+                if distance < rule.NEIGHBOURHOOD:
+                    rule.accumulate(self, other, distance)
         self.adjustment = [0.0, 0.0, 0.0]  # reset adjustment vector
-        for correction in corrections:  # save corrections to the adjustment
-            correction.add_adjustment(self)
+        for rule in rules:  # save corrections to the adjustment
+            rule.add_adjustment(self)
 
     def limit_speed(self, max_speed):
         """
@@ -169,14 +161,13 @@ class Boid:
         """
         Move to new position
         """
-        velocity = self.velocity + self.adjustment  # note: += is buggy
+        self.velocity += self.adjustment
         # Hack: Add a constant velocity in whatever direction
         # they are moving so they don't ever stop.
-        if velocity.length() > 0:
-            velocity = velocity + (velocity.normalize() * random_range(0.0, 0.007))
-        self.velocity = velocity
+        if self.velocity.length() > 0:
+            self.velocity += (self.velocity.normalize() * random.uniform(0.0, 0.007))
         self.limit_speed(1.0)
-        self.location = self.location + self.velocity  # note += is buggy
+        self.location += self.velocity
 
 
 class Flock:
@@ -188,8 +179,18 @@ class Flock:
         self.cube0 = cube0  # cube min vertex
         self.cube1 = cube1  # cube max vertex
         self.boids = []
-        for _ in itertools.repeat(None, num_boids):
+        for _ in range(num_boids):
             self.boids.append(Boid())
+
+    def update(self):
+        """
+        update flock positions
+        """
+        for boid in self.boids:
+            boid.orient(self.boids)  # calculate new velocity
+        for boid in self.boids:
+            boid.update()  # move to new position
+            boid.wrap(self.cube0, self.cube1)
 
     def __repr__(self):
         rep = "Flock of %d boids bounded by %s, %s:\n" % (
@@ -238,16 +239,6 @@ class Flock:
             [self.cube1.x, self.cube0.y, self.cube1.z],
         ]
 
-    def update(self):
-        """
-        update flock positions
-        """
-        for boid in self.boids:
-            boid.orient(self.boids)  # calculate new velocity
-        for boid in self.boids:
-            boid.update()  # move to new position
-            boid.wrap(self.cube0, self.cube1)
-
     def render(self):
         """
         draw a flock of boids
@@ -289,9 +280,9 @@ class Flock:
 
 def main():
     # initialize pygame and setup an opengl display
-    ## angle = 0.4  # camera rotation angle
+    #  angle = 0.4  # camera rotation angle
     angle = 0.1  # camera rotation angle
-    ## side = 700
+    # side = 700
     side = 1000
     delay = 0  # time to delay each rotation, in ms
     xyratio = 1.0  # == xside / yside
